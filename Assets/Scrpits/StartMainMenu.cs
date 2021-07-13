8a6b0c43b9d18a76c96caaa136f386d9cbc160b5
Qianchen-Zhang <65546808+Qianchen-Zhang@users.noreply.github.com>
2021-07-13 14:55:17 +0200

    13/07/2021 Third update
    Updated some scenes and transitions between scenes

--- Assets/Scrpits/StartMainMenu.cs
+++ Assets/Scrpits/StartMainMenu.cs
@@ -2,25 +2,84 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartMainMenu : MonoBehaviour
 {
+    public Image mask;
+    private float alpha;
+    public GameObject maskObject;
+
+    public void Start()
+    {
+        StartCoroutine(FadeIn());
+        
+    }
+
+
+    public void StartButton()
+    {
+        StartCoroutine(FadeOut("waitingHall"));
+    }
+
+
+    public void QuitButton()
+    {
+        StartCoroutine(FadeOut());
+       
+    }
+
+    public void SettingButton()
+    {      
+        StartCoroutine(FadeOut("setting"));        
+    }
     
 
-    public void QuitGame()
+    IEnumerator FadeIn()
     {
+        alpha = 1;
+        while (alpha > 0)
+        {
+            alpha -= Time.deltaTime;
+            mask.color = new Color(0, 0, 0, alpha);
+            yield return new WaitForSeconds(0);
+        }
+        maskObject.SetActive(false);
+    }
+
+
+    IEnumerator FadeOut(string sceneName)
+    {
+        maskObject.SetActive(true);
+        alpha = 0;
+        while (alpha<1)
+        {
+            alpha += Time.deltaTime;
+            mask.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+        SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator FadeOut()
+    {
+        maskObject.SetActive(true);
+        alpha = 0;
+        while (alpha < 1)
+        {
+            alpha += Time.deltaTime;
+            mask.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+
         #if UNITY_EDITOR
                 UnityEditor.EditorApplication.isPlaying = false;
         #else
-                Application.Quit();
+                        Application.Quit();
      
         #endif
+
     }
 
-    public void goToSettingMenu()
-    {
-        SceneManager.LoadScene("setting");
-    }
-    
 
 }
--- Assets/Scrpits/setMenu.cs
+++ Assets/Scrpits/setMenu.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class setMenu : MonoBehaviour
 {
@@ -9,8 +10,15 @@ public class setMenu : MonoBehaviour
     private bool isChange = false;
     private bool isConfirm ;
 
-   
+    public Image mask;
+    private float alpha;
+    public GameObject maskObject;
 
+
+    public void Start()
+    {
+        StartCoroutine(FadeIn());
+    }
 
     public void confirmSet()
     {
@@ -22,7 +30,7 @@ public class setMenu : MonoBehaviour
     {
         //if (!isChange) isConfirm = true; //当功能完成时设置
 
-        if(isConfirm) SceneManager.LoadScene("start");
+        if(isConfirm) StartCoroutine(FadeOut("start"));
         else confirmPromptInterface.SetActive(true);
     }
 
@@ -34,7 +42,53 @@ public class setMenu : MonoBehaviour
 
     public void noButton()
     {
-        SceneManager.LoadScene("start");
+        StartCoroutine(FadeOut("start"));
+    }
+
+
+    IEnumerator FadeIn()
+    {
+        alpha = 1;
+        while (alpha > 0)
+        {
+            alpha -= Time.deltaTime;
+            mask.color = new Color(0, 0, 0, alpha);
+            yield return new WaitForSeconds(0);
+        }
+        maskObject.SetActive(false);
+    }
+
+    IEnumerator FadeOut(string sceneName)
+    {
+        maskObject.SetActive(true);
+        alpha = 0;
+        while (alpha < 1)
+        {
+            alpha += Time.deltaTime;
+            mask.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+        SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator FadeOut()
+    {
+        maskObject.SetActive(true);
+        alpha = 0;
+        while (alpha < 1)
+        {
+            alpha += Time.deltaTime;
+            mask.color = new Color(0, 0, 0, alpha);
+            yield return null;
+        }
+
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                                Application.Quit();
+     
+        #endif
+
     }
 
 }
